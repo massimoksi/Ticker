392c9dac49d6d180e519f5c520fc497bde25803c
Massimo Peri <massimoperi@gmail.com>
2016-01-22 23:51:36 +0100

    Initial Commit

---
+++ Ticker/Ticker.h
@@ -0,0 +1,19 @@
+//
+//  Ticker.h
+//  Ticker
+//
+//  Created by Massimo Peri on 22/01/16.
+//  Copyright © 2016 Massimo Peri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ticker.
+FOUNDATION_EXPORT double TickerVersionNumber;
+
+//! Project version string for Ticker.
+FOUNDATION_EXPORT const unsigned char TickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ticker/PublicHeader.h>
+
+
